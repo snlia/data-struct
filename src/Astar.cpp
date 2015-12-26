d01a6bde79385d2fe941bd48d34e32e7716d0d07
snlia <chinsnlia@gmail.com>
2015-12-26 19:44:59 +0800

    Astar	新文件：   src/Astar.cpp

---
+++ src/Astar.cpp
@@ -0,0 +1,33 @@
+#include "common.h"
+#include "Node.h"
+
+typedef std::pair <double, ll> pdi;
+static std::priority_queue <pdi, std::vector <pdi>, std::greater <pdi> > Q;
+
+void Astar (ll s, ll t)
+{
+    node.reset ();
+    node[s].adj = 0;
+    while (!Q.empty ()) Q.pop ();
+    Q.push (std::make_pair (0, s));
+    while (!Q.empty ())
+    {
+        ll x = Q.top ().second;
+        if (x == t) {printf ("%.7lfm\n", node[t].adj); return ;}
+        Q.pop ();
+        if (node[x].vis) continue;
+        node[x].vis = 1;
+        double now_dis = node[x].adj;
+        for (int i = 0; i < (int) node[x].edge.size (); ++i)
+        {
+            ll tx = node[x].edge[i].first;
+            if (node[tx].adj > now_dis + node[x].edge[i].second)
+            {
+                node[tx].fa = x;
+                node[tx].adj = now_dis + node[x].edge[i].second;
+                Q.push(std::make_pair (node[tx].adj + node[tx].dis (t), tx));
+            }
+        }
+    }
+}
+
