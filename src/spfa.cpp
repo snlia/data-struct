e633e933707d0a4f91a8aefc97dc7148bac0b61a
snlia <chinsnlia@gmail.com>
2015-12-26 19:42:59 +0800

    关于路径压缩的实现，进行了改动	修改：     src/Node.cpp 正确实现了路径压缩	修改：     src/spfa.cpp

--- src/spfa.cpp
+++ src/spfa.cpp
@@ -13,7 +13,7 @@ void spfa (ll s, ll t)
         ll x = Q.front (); Q.pop_front ();
         node[x].vis = 0;
         double now_dis = node[x].adj;
-        if (getf (x) == x && node[x].edge.size () == 1 && getf (x) != getf (node[x].edge[0].first))
+        if (getf (x) == x && node[x].edge.size () == 1 && x != getf (node[x].edge[0].first))
         {
             node[x].Fa = node[x].edge[0].first;
             node[x].fadj = node[x].edge[0].second;
@@ -24,7 +24,7 @@ void spfa (ll s, ll t)
             double Dis = node[x].edge[i].second + now_dis + node[node[x].edge[i].first].fadj;
             if (Dis < node[tx].adj)
             {
-                node[tx].fa = x;
+                node[tx].fa = getf (node[x].edge[i].first) == node[x].edge[i].first ? x : node[node[x].edge[i].first].Fa2;
                 node[tx].adj = Dis;
                 if (!node[tx].vis && node[tx].adj + node[tx].dis (t) < node[t].adj)
                 {
